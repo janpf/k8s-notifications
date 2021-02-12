c0dd743830c16ccbcba42ef57a988fd096d49b75
Jan Pfister <janpf@users.noreply.github.com>
2021-02-12 18:00:57 +0100

    pretty print messages

--- notifications.py
+++ notifications.py
@@ -5,6 +5,23 @@ from pathlib import Path
 
 
 logger = logging.getLogger("notification_manager")
+config_file = Path.home() / ".nofconfig"
+
+
+class _NotificationChannel(object):
+    def __init__(self):
+        raise NotImplementedError
+
+    def notify(self, event: str) -> None:
+        raise NotImplementedError
+
+    def pprint(self, event: Dict) -> str:
+        """
+        formats the kubernetes event into a nicely readable string
+        :param event:
+        :return:
+        """
+        return f"{event['type']}: {event['object'].kind} {event['object'].metadata.name} ({event['object'].metadata.owner_references[0].kind} {event['object'].metadata.owner_references[0].name})"
 
 
 class NotificationManager(object):
@@ -13,32 +30,22 @@ class NotificationManager(object):
         logger.debug(f"found channels: {self.notification_channels}")
 
     @staticmethod
-    def _getChannel(channelName: str):
+    def _getChannel(channelName: str) -> _NotificationChannel:
         import notifications as notif
 
-        cls: notif._NotificationChannel = getattr(notif, "_" + channelName)()
-        return cls
+        return getattr(notif, "_" + channelName)()
 
-    def notify(self, event):
-        # TODO clean message
+    def notify(self, event) -> None:
         for channel in self.notification_channels:
             logger.debug(f"notifying {channel} about {event['type']}")
-            channel.notify(event)
-
-
-class _NotificationChannel(object):
-    def __init__(self):
-        raise NotImplementedError
-
-    def notify(self, event: Dict[str, str]):
-        raise NotImplementedError
+            channel.notify(channel.pprint(event))
 
 
 class _rocketchat(_NotificationChannel):
     def __init__(self):
         logger.debug(f"{type(self)} instantiated")
         config = configparser.ConfigParser()
-        config.read(Path.home()/ ".nofconfig")
+        config.read(config_file)
         self.config: Dict[str, str] = config["rocketchat"]
         self._new_rc_connection()
         logger.debug(f"{self.config['username']} will notify {self.config['yourUsername']}")
@@ -53,9 +60,11 @@ class _rocketchat(_NotificationChannel):
                 "domain": self.config["server"],
             }
         )
+        logging.getLogger("base").setLevel(logging.WARN) # FIXME still logs on debug level
+
         self.im_room = self.rc.create_im_room(self.config["yourUsername"])
 
-    def notify(self, event: Dict[str, str]):
+    def notify(self, event: str):
         try:
             self.rc.send_message(message=str(event), room_id=self.im_room["id"])
         except:
@@ -70,14 +79,9 @@ class _webhook(_NotificationChannel):
     def __init__(self):
         logger.debug(f"{type(self)} instantiated")
         config = configparser.ConfigParser()
-        config.read(Path.home()/ ".nofconfig")
+        config.read(config_file)
         self.config: Dict[str, str] = config["webhook"]
         logger.info(f"webhook url: {self.config['url']}")
 
-    def notify(self, event: Dict[str, str]):
-        pass
-
-
-class _discordwebhook(_webhook):
-    def notify(self, event: Dict[str, str]):
-        pass
+    def notify(self, event: str):
+        raise NotImplementedError()
