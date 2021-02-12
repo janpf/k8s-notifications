0bb5b6d343130ac5de5dc348f57d1ca890647b68
Jan Pfister <janpf@users.noreply.github.com>
2021-02-12 18:00:16 +0100

    argparse support and sensible defaults

--- k8s-watcher.py
+++ k8s-watcher.py
@@ -1,27 +1,40 @@
 import logging
 from kubernetes import client, config, watch
 from notifications import NotificationManager
+import argparse
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 logger = logging.getLogger("k8s_watch")
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
 w = watch.Watch()
 
-# TODO make argparse settable
-#namespace = config.kube_config.list_kube_config_contexts()[0][0]["context"]["namespace"]
-namespace = config.kube_config.list_kube_config_contexts()[0][0]["context"]["user"]
-events_to_notify = ["ERROR", "DONE", "ADDED"]
+try:
+    namespace = config.kube_config.list_kube_config_contexts()[0][0]["context"]["namespace"]
+except:
+    try:
+        namespace = config.kube_config.list_kube_config_contexts()[0][0]["context"]["user"]
+    except:
+        namespace = ""
+
+events_to_notify = ["ERROR", "DONE"] #  "ADDED"
 ways_to_notify = ["rocketchat"]  # rocketchat, discord, telegram, email, whatev
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--namespace", type=str, default=namespace, help="the namespace to watch for changes")
+parser.add_argument("--events_to_notify", metavar="N", type=str, nargs="+", default=events_to_notify, help="ERROR DONE ADDED")
+parser.add_argument("--notification_channels", metavar="N", type=str, nargs="+", default=ways_to_notify, help="implement new ones in notifications.py")
 
 logger.info(f"watching namespace {namespace}")
 logger.info(f"notification triggers {events_to_notify}")
 logger.info(f"notification channels {ways_to_notify}")
 
 not_man = NotificationManager(ways_to_notify)
-
 logger.info("start watching cluster")
 while True:
     for e in w.stream(v1.list_namespaced_pod, namespace=namespace):
@@ -31,3 +44,5 @@ while True:
         logger.info(f"Event '{e['type']}' for pod '{e['object'].metadata.name}'")
         if e["type"] in events_to_notify:
             not_man.notify(e)
+            # TODO v1.read_namespaced_pod_log()
+            # append last 5 lines of logs
