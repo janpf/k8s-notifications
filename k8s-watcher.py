85728961759d4ee99c4e70f94a6edda31a960e3e
Jan Pfister <janpf@users.noreply.github.com>
2021-02-12 11:24:45 +0100

    defaults for namespace setting

--- k8s-watcher.py
+++ k8s-watcher.py
@@ -1,33 +1,29 @@
-import argparse
 import logging
-
 from kubernetes import client, config, watch
-
 from notifications import NotificationManager
-
-config.load_kube_config()
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger("k8s_watch")
 
+config.load_kube_config()
 v1 = client.CoreV1Api()
 w = watch.Watch()
 
-
-namespace = "studpfister"
+# TODO make argparse settable
+namespace = config.kube_config.list_kube_config_contexts()[0][0]["context"]["namespace"]
 events_to_notify = ["ERROR", "DONE", "ADDED"]
-ways_to_notify = ["webhook"]  # rocket chat, discord, telegram, email, whatev
+ways_to_notify = ["rocketchat"]  # rocketchat, discord, telegram, email, whatev
 
 
 logger.info(f"watching namespace {namespace}")
-logger.info(f"going to emit a notification for {events_to_notify}")
-logger.info(f"going to send to the channels {ways_to_notify}")
+logger.info(f"notification triggers {events_to_notify}")
+logger.info(f"notification channels {ways_to_notify}")
 
 not_man = NotificationManager(ways_to_notify)
 
 logger.info("start watching cluster")
 while True:
-    for e in w.stream(v1.list_namespaced_pod, namespace="studpfister"):
+    for e in w.stream(v1.list_namespaced_pod, namespace=namespace):
         if e["type"] == "ERROR" and e["raw_object"]["message"].contains("too old resource"):
             logger.warning("Going to restart as 'watch' lost connection. Going to relist all existing pods:")
             continue
