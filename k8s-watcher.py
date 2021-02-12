b02550190cd1a98c4f06df04be000d1ca88d3e24
Jan Pfister <janpf@users.noreply.github.com>
2021-02-12 21:06:00 +0100

    fixed argparse

--- k8s-watcher.py
+++ k8s-watcher.py
@@ -22,19 +22,21 @@ except:
     except:
         namespace = ""
 
-events_to_notify = ["ERROR", "DONE"] #  "ADDED"
-ways_to_notify = ["rocketchat"]  # rocketchat, discord, telegram, email, whatev
+default_events_to_notify = ["ERROR", "DONE"] #  "ADDED"
+default_ways_to_notify = ["rocketchat"]  # rocketchat, discord, telegram, email, whatev
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--namespace", type=str, default=namespace, help="the namespace to watch for changes")
-parser.add_argument("--events_to_notify", metavar="N", type=str, nargs="+", default=events_to_notify, help="ERROR DONE ADDED")
-parser.add_argument("--notification_channels", metavar="N", type=str, nargs="+", default=ways_to_notify, help="implement new ones in notifications.py")
+parser.add_argument("--events_to_notify", type=str, nargs="+", default=default_events_to_notify, help="ERROR DONE ADDED")
+parser.add_argument("--notification_channels", type=str, nargs="+", default=default_ways_to_notify, help="implement new ones in notifications.py")
 
-logger.info(f"watching namespace {namespace}")
-logger.info(f"notification triggers {events_to_notify}")
-logger.info(f"notification channels {ways_to_notify}")
+pargs = parser.parse_args()
 
-not_man = NotificationManager(ways_to_notify)
+logger.info(f"watching namespace {pargs.namespace}")
+logger.info(f"notification triggers {pargs.events_to_notify}")
+logger.info(f"notification channels {pargs.notification_channels}")
+
+not_man = NotificationManager(pargs.notification_channels)
 logger.info("start watching cluster")
 while True:
     for e in w.stream(v1.list_namespaced_pod, namespace=namespace):
@@ -42,7 +44,7 @@ while True:
             logger.warning("Going to restart as 'watch' lost connection. Going to relist all existing pods:")
             continue
         logger.info(f"Event '{e['type']}' for pod '{e['object'].metadata.name}'")
-        if e["type"] in events_to_notify:
+        if e["type"] in pargs.events_to_notify:
             not_man.notify(e)
             # TODO v1.read_namespaced_pod_log()
             # append last 5 lines of logs
