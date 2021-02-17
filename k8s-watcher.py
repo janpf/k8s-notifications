53621b118d5c7b03d1f6bb00cfaf090df7f0b42c
Jan Pfister <janpf@users.noreply.github.com>
2021-02-17 12:59:33 +0100

    un-debug

--- k8s-watcher.py
+++ k8s-watcher.py
@@ -4,7 +4,7 @@ from notifications import NotificationManager
 import argparse
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
